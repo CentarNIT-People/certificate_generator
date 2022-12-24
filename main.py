f134585075fae97a06eae115ab9bcfc901830a5a
Hamza Plojovic <hamzaplojovic9@gmail.com>
2022-12-24 12:31:30 +0100

    test:

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import os
 import convertapi
 from docxtpl import DocxTemplate
 
-def convert_to_pdf():
-    os.chdir("results")
-    for file in os.listdir():
-        if file.endswith(".docx"):
-            convertapi.api_secret = '59fywzlChxa3iNc7'
-            convertapi.convert('pdf', {
-                'File': file
-            }, from_format = 'docx').save_files('../pdf')
-    os.chdir("..")
+# def convert_to_pdf():
+#     os.chdir("results")
+#     for file in os.listdir():
+#         if file.endswith(".docx"):
+#             convertapi.api_secret = '59fywzlChxa3iNc7'
+#             convertapi.convert('pdf', {
+#                 'File': file
+#             }, from_format = 'docx').save_files('../pdf')
+#     os.chdir("..")
 
 def excecute(list_of_names, course_name, group_name, group_id, date):
     # template = input("Enter template name: ")
     for index, name in enumerate(list_of_names):
         file_name = str(name).replace(" ", "_")
-        doc = DocxTemplate("./templates/nit_python.docx")
+        doc = DocxTemplate("./templates/gimnazija_python.docx")
         context = { 
             "name" : name,
             "id" :f"1.{index+1}",
@@ -32,6 +32,6 @@ def excecute(list_of_names, course_name, group_name, group_id, date):
         os.chdir("results")
         doc.save(f"{file_name}.docx")
         os.chdir("..")
-    convert_to_pdf()
+    # convert_to_pdf()
     return "Done"
 
